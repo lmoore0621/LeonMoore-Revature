5b23ed588c72ff8e7bdb68b7fb438a6ee21d3bbc
LeonMoore <leon.moore90@gmail.com>
2017-10-06 15:05:38 -0400

    angular configuration

--- Week6/angularize/script/ngapp.js
+++ Week6/angularize/script/ngapp.js
@@ -1,4 +1,21 @@
 (function (na) {
     'use strict';
-    
+
+    var hw = angular.module('HelloWorld', ['ngRoute']);
+
+    hw.config(['$routeProvider', function (banana) {
+        banana
+            .when('/home', {
+                controller: 'homeCtrl',
+                templateUrl: 'home/template.html'
+        })
+        .when('/contact', {
+            controller: 'contact',
+            templateUrl: 'contact/template.html'
+        })
+        .otherwise({
+            redirectToUrl: '/home'
+        });
+    }]);
+
 })(window.ngApp || (window.ngApp = {}));
